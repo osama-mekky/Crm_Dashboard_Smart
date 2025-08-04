64386305c6b34585c0ee2fb1571e44c2bc9992cf
osama-mekky <osama437.meky@gmail.com>
2025-08-04 11:32:11 +0300

    Add cover new

--- crm_dashboard_smart/__manifest__.py
+++ crm_dashboard_smart/__manifest__.py
@@ -23,7 +23,7 @@
     'version': '17.0.1.0.0',
 
     'images': [
-        'static/description/icon.png',       # Main icon (also used as cover if no cover specified)
+        # 'static/description/icon.png',       # Main icon (also used as cover if no cover specified)
         'static/description/cover.png',  # Example screenshot
     ],
     
