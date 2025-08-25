6ad5ad045f6e25d97c48851c4705e4b4c869907f
osama-mekky <85651213+osama-mekky@users.noreply.github.com>
2025-08-25 10:52:22 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -13,7 +13,7 @@
 	""",
 
     'author': "Osama Mekky",
-    'website': "https://gosmart.eg",
+    'website': "https://gosmart.egaa",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
